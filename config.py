7522a1564614f1836bcce1e24bec857ad9660aa5
duxlong <duxlong@me.com>
2022-01-05 13:27:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=7Yc6TXxYl3EWxFUFEXATF46; r_sort_type=score; __jsluid_s=a16d1984aa6f00931f88fac77b7d65c7; smzdm_user_source=628836A58C04A0BF3DA9A2CC87F1BE6A; shequ_pc_sug=b; device_id=101760723216235099113246849a100dd0285b4bb1f0ef2055f78dfe27; userId=user:2822455733|2822455733; homepage_sug=c; _ga=GA1.2.564349193.1618910975; _ga_09SRZM2FDD=GS1.1.1636948859.25.1.1636949372.0; footer_floating_layer=0; ad_date=5; ad_json_feed=%7B%7D; _zdmA.vid=*; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1641358576; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%222822455733%22%2C%22first_id%22%3A%22178ee9d12b81a7-0ef44c582833cb-52301943-4953600-178ee9d12b965d%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fgithub.com%2Fduxlong%2Fsmzdm_bot%22%7D%2C%22%24device_id%22%3A%22178ee9d12b81a7-0ef44c582833cb-52301943-4953600-178ee9d12b965d%22%7D; sess=AT-wYOJv%2F00oAtHFGGjlHKpcQTqWIhOGtmJvLj5uiF%2B62NgQdPp7AoVvoCg2sPEMDtUEzBm3sFjh7TZB8mdfPDI77ozL4L3MgceFzp9zUSj7OUSWsWleaMIh96R; user=user%3A2822455733%7C2822455733; smzdm_id=2822455733; _zdmA.uid=ZDMA.7bJTvjDoB.1641358627.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1641358627; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; _zdmA.time=1641358648351.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
